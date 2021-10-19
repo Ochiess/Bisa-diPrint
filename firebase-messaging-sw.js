53f26a0c02e0febf21221d72cf9bdc9da13a832d
Rahmat Ilyas <rahmat.ilyas142@gmail.com>
2021-10-19 23:14:57 +0800

    rev bug

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,4 +1,7 @@
-const firebaseConfig = {
+importScripts('https://www.gstatic.com/firebasejs/8.1.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.1.1/firebase-messaging.js');
+
+firebase.initializeApp({
   apiKey: "AIzaSyAju5B0X0LZPoPD0GEMgHkX4cVVuuexvoA",
   authDomain: "diprint.firebaseapp.com",
   databaseURL: "https://diprint-default-rtdb.firebaseio.com",
@@ -6,25 +9,8 @@ const firebaseConfig = {
   storageBucket: "diprint.appspot.com",
   messagingSenderId: "668022140321",
   appId: "1:668022140321:web:c24e877b14162f25846fe0"
-};
+});
 
 // firebase.initializeApp(firebaseConfig);
 
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.2/firebase-app.js";
-import { getMessaging, getToken, onMessage } from "https://www.gstatic.com/firebasejs/9.0.2/firebase-messaging.js";
-import { onBackgroundMessage } from "https://www.gstatic.com/firebasejs/9.0.2/firebase-messaging-sw.js";
-
-const firebase = initializeApp(firebaseConfig)
-const messaging = getMessaging();
-
-onBackgroundMessage(messaging, (payload) => {
-  console.log('[firebase-messaging-sw.js] Received background message ', payload);
-  // Customize notification here
-  const notificationTitle = 'Background Message Title';
-  const notificationOptions = {
-    body: 'Background Message body.',
-    icon: '/firebase-logo.png'
-  };
-
-  self.registration.showNotification(notificationTitle, notificationOptions);
-});
+const messaging = firebase.messaging();
