03ab4a9568c13f823a2f97ab535b8ec4d666b901
Rahmat Ilyas <rahmat.ilyas142@gmail.com>
2023-02-05 21:11:49 +0800

    firebase config

--- firebase/index.php
+++ firebase/index.php
@@ -3,18 +3,19 @@
 <script src="https://www.gstatic.com/firebasejs/8.2.9/firebase-auth.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/js-cookie@2/src/js.cookie.min.js"></script>
 
-<script>
+<script> 
     const firebaseConfig = {
-        apiKey: "AIzaSyAju5B0X0LZPoPD0GEMgHkX4cVVuuexvoA",
-        authDomain: "diprint.firebaseapp.com",
-        projectId: "diprint",
-        storageBucket: "diprint.appspot.com",
-        messagingSenderId: "668022140321",
-        appId: "1:668022140321:web:790e3e02afa0e7d0846fe0"
+        apiKey: "AIzaSyBB5zjP-Fr4z7ExLCWT8I8jHCg7PnfXRnA",
+        authDomain: "diprint-online.firebaseapp.com",
+        projectId: "diprint-online",
+        storageBucket: "diprint-online.appspot.com",
+        messagingSenderId: "979321405610",
+        measurementId: "G-7SFVLP210Q"
+        appId: "1:979321405610:web:ec7080e22eb7945b3c90d3",
     };
 
     // Initialize Firebase
     firebase.initializeApp(firebaseConfig);
 
-    
+
 </script>
